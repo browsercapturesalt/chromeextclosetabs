22c76982e21c2fe115db9ea8be53c2defb1d26f7
hyperbotauthor <hyperbotauthor@gmail.com>
2022-02-01 19:12:56 +0100

    Working

--- background.js
+++ background.js
@@ -1,4 +1,21 @@
+function checkTabs() {
+  const urls = {};
+  chrome.tabs.getAllInWindow(undefined, (tabs) => {
+    for (const tab of tabs) {
+      if (urls[tab.url]) {
+        urls[tab.url]++;
+        console.log("removing", tab.url, "multipilicity", urls[tab.url]);
+        chrome.tabs.remove(tab.id);
+      } else {
+        urls[tab.url] = 1;
+      }
+    }
+  });
+}
+
 // https://developer.chrome.com/docs/extensions/reference/runtime/#event-onInstalled
 chrome.runtime.onInstalled.addListener(() => {
   console.log("Runtime initialized.");
+  checkTabs();
+  setInterval(checkTabs, 1000);
 });
